ac781e9ca2f6eaba7942818979907873a54c8d65
Siael Alves <siaelalves@outlook.com>
2024-09-30 16:08:35 -0300

    Excluída classe session do carregamento;

--- session.php
+++ session.php
@@ -36,9 +36,9 @@ class session {
   * @return bool Retorna True se a sessão estiver aberta e False não houver sessão.
   */
  public function exists ( ) : bool {
-
-  if ( isset ( $_SESSION [ "id"] ) ) {
-
+  
+  if ( session_status ( ) == PHP_SESSION_ACTIVE ) {
+   
    if ( $_SESSION [ "id" ] == $this->get_hash ( ) ) {
 
     return true ;
@@ -58,9 +58,11 @@ class session {
 
   if ( $this->exists ( ) ) {
    
+   session_start ( ) ;
    $this->id = $_SESSION [ "id" ] ;
    $this->username = $_SESSION [ "username" ] ;
    $this->password = $_SESSION [ "password" ] ;
+   session_id ( $this->id ) ;
 
   } else {
 
@@ -69,6 +71,49 @@ class session {
   }
 
  }
+ 
+ /**
+  * Inicializa uma nova sessão com um novo id baseado no usuário e senha especificados.
+  */
+  public function create_session ( ) {
+
+   session_id ( $this->id ) ;
+   session_start ( );
+   $_SESSION [ "id" ] = $this->id ;
+   $_SESSION [ "username" ] = $this->username ;
+   $_SESSION [ "password" ] = $this->password ;   
+ 
+  }
+
+  public function destroy_session ( ) {
+
+   $this->id = "" ;
+   $this->username = "" ;
+   $this->password = "" ;
+   session_destroy ( ) ;
+
+  }
+
+  /**
+   * Verifica se o login dessa sessão é válido ou não.
+   * @return bool Retorna True se o login existir no banco de dados. 
+   * Retorna False se não existir.
+   */
+  public function is_valid ( ) : bool {
+
+   if ( $this->username == "siaelalves" ) {
+
+    if ( $this->password == "siaelcode" ) {
+
+      return true ;
+
+    }
+
+   }
+
+   return false ;
+
+  }
 
  /**
   * Gera uma hash única para uma sessão de usuário.
@@ -76,17 +121,6 @@ class session {
  private function get_hash ( ) : string {
 
   return hash ( "md5" , $this->username . $this->password ) ;
-
- }
-
- /**
-  * Inicializa uma nova sessão com um novo id baseado no usuário e senha especificados.
-  */
- public function create_session ( ) {
-
-  $_SESSION [ "id" ] = $this->id ;
-  $_SESSION [ "username" ] = $this->username ;
-  $_SESSION [ "password" ] = $this->password ;
 
  }
 
