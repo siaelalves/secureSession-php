860e30da256e75926aa976f55ba68139262d95a2
Siael Alves <siaelalves@outlook.com>
2025-03-16 15:22:54 -0300

    FEAT Criado inicializador para submódulo `security`;

---
+++ security.php
@@ -0,0 +1,16 @@
+<?php
+namespace security ;
+
+require dirname(__FILE__) . "/oauth.php" ;
+
+require dirname(__FILE__) . "/token.php" ;
+
+require dirname(__FILE__) . "/GRANT_TYPE.php" ;
+
+require dirname(__FILE__) . "/KEYS.php" ;
+
+require dirname(__FILE__) . "/TOKEN_TYPE.php" ;
+
+require dirname(__FILE__) . "/REQUEST_METHOD.php" ;
+
+require dirname(__FILE__) . "/URLs.php" ;
